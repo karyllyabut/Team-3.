fe0c5c0d61e18137a893ba7194174258a1b34cce
JESSIE-19 <jmc.eseh.29@gmail.com>
2022-01-25 04:07:19 +0800

    Create update-password.php

---
+++ Food-Ordering/admin/update-password.php
@@ -0,0 +1,20 @@
+<?php include('partials/menu.php'); ?>
+
+<div class="main-content">
+  <div class="wrapper">
+    <h1>Change Password</h1>
+    <br><br>
+
+    <form action="" method="POST"></form>
+
+      <table class="tbl-30">
+        <tr>
+          <td>Old Password</td>   
+        </tr>
+
+      </table>
+
+  </div>
+</div>
+
+<?php include('partials/footer.php'); ?>
