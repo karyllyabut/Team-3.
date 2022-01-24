89c6666f83c3dc9789062a5df18a4013db70a87d
JESSIE-19 <jmc.eseh.29@gmail.com>
2022-01-25 01:36:30 +0800

    Update footer.php

--- Food-Ordering/admin/partials/footer.php
+++ Food-Ordering/admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer Section Starts -->
 <div class="footer">
   <div class="wrapper">
-    <p class="text-center">2022 All rights reserved, Some Restaurant Develop By Team 3</p>
+    <p class="text-center">2022 All rights reserved, Ordering System Develop By Team 3</p>
   </div>
 </div>
 <!-- Footer Section Ends -->
