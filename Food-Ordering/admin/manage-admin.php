8f9e242eb100946be368ca2115c7540bbf82d166
JESSIE-19 <jmc.eseh.29@gmail.com>
2022-01-25 04:07:15 +0800

    Update manage-admin.php

--- Food-Ordering/admin/manage-admin.php
+++ Food-Ordering/admin/manage-admin.php
@@ -19,6 +19,13 @@
             echo $_SESSION['delete'];
             unset ($_SESSION['delete']);
           }
+
+          if (isset ($_SESSION['update']))
+          {
+            echo $_SESSION['update'];
+            unset ($_SESSION['update']);
+          }
+
          ?>
          <br><br><br>
         <!-- Buttom to admin -->
@@ -69,6 +76,7 @@
                       <td><?php echo $full_name; ?></td>
                       <td><?php echo $username; ?></td>
                       <td>
+                        <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>" class="btn-primary">Change Password</a>
                         <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
                         <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
                       </td>
