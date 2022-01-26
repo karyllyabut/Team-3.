e5ac2230417bb288d8ca943aad75720ea1c88b27
DanicaRollque <danicamae.rolluque@gmail.com>
2022-01-26 23:57:02 +0800

    Update add-category.php

--- Food-Ordering/admin/add-category.php
+++ Food-Ordering/admin/add-category.php
@@ -6,8 +6,24 @@
 
         <br><br>
 
+        <?php
+            if(isset($_SESSION['add']))
+            {
+                echo $_SESSION['add'];
+                unset($_SESSION['add']);
+            }
+
+            if(isset($_SESSION['upload']))
+            {
+                echo $_SESSION['upload'];
+                unset($_SESSION['upload']);
+            }
+        ?>
+
+        <br><br>
+         
         <!-- Add category form starts -->
-        <form action="" method="POST"> 
+        <form action="" method="POST" enctype="multipart/form-data"> 
 
             <table class="tbl-30">
                 <tr>
@@ -15,6 +31,13 @@
                      <td>
                          <input type="text" namer="title" placeholder="Category Title">
                      </td>
+                </tr>
+
+                <tr>
+                    <td>Select Image: </td>
+                    <td>
+                        <input type="file" name="image">
+                    </td>
                 </tr>
 
                 <tr>
@@ -73,10 +96,47 @@
                 {
                     $active = "No";
                 }
+                
+                //Check whether the image is selected or not and set the value for image name accordingly
+                //print_r($_FILES['image']);
+
+                //die();//Break the code here 
+
+                if(isset($_FILES['image']['name']))
+                {
+                    //upload the image
+                    // To upload image  we need image name, source path and destination path
+                    $image_name = $_FILES['image']['name'];
+
+                    $source_path = $_FILES['image']['tmp_name'];
+
+                    $destination_path = "../images/category/".$image_name;
+
+                    //Finally upload the image
+                    $upload = move_uploaded_file($source_path, $destination_path);
+
+                    //Check the image if uploaded or not
+                    //And if the image is not uploaded then we will stop the process and redirect with error message 
+                    if($upload==false)
+                    {
+                        $_SESSION['upload'] = "<div class='error'>Failed to upload image. </div>";
+                        //Redirect to Add  Category Page
+                        header('location:'.SITEURL.'admin/add-category.php');
+                        //Stop the process
+                        die();
+                    }
+
+                }
+                else
+                {
+                    //Don't upload image and set the image_name value as blank
+                    $image_name="";
+                }
 
                 //create SQL Query to insert category into database
                 $sql = "INSERT INTO tbl_category SET
                     title='$title', 
+                    image_name='$image_name',
                     featured='$featured',
                     active ='$active'                
                 ";
@@ -85,7 +145,20 @@
                 $res = mysqli_query($conn, $sql);
 
                 /// check whether the  query  executedc or not and dat5a added or not
-                if(4res==true)
+                if($res==true)
+                {
+                    //Query executed and category added
+                    $_SESSION['add'] = "<div class='success'>Category Added Successfully.</div>";
+                    //redirect to manage category paqge 
+                    header('location:'.SITEURL.'admin/manage-category.php');
+                }
+                else
+                {
+                    //Failed to add category
+                    $_SESSION['add'] = "<div class='error'>Failed to add Category.</div>";
+                    //redirect to manage category paqge 
+                    header('location:'.SITEURL.'admin/add-category.php');
+                }
             }
 
 
